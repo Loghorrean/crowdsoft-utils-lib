baf155002a3bc8e40fe383a235d008766fb7dbbc
Loghorrean <loghorrean74@gmail.com>
2023-10-20 13:36:14 +0300

    Added more levels of parseErrorMessages function

--- src/utils/errors/parseErrorMessages.ts
+++ src/utils/errors/parseErrorMessages.ts
@@ -1,13 +1,21 @@
-import {isNotEmpty} from "../functions";
+import { isValueEmpty } from "../functions";
+import { ValidationErrors } from "../types";
 
 type FormErrors = {
-    [key: string]: Array<string> | undefined;
+    [key: string]: ValidationErrors | FormErrors | undefined;
 };
 
 export const parseErrorMessages = (errors: FormErrors): Array<string> => {
     const messages: Array<string> = [];
     for (const error of Object.values(errors)) {
-        isNotEmpty(error) && messages.push(...error);
+        if (isValueEmpty(error)) {
+            continue;
+        }
+        if (Array.isArray(error)) {
+            messages.push(...error);
+            continue;
+        }
+        messages.push(...parseErrorMessages(error));
     }
     return messages;
 };
