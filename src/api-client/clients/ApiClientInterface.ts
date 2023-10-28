39e0418438204a6a2cf26b4bbda8fa68a31a305b
Loghorrean <loghorrean74@gmail.com>
2023-10-28 21:20:42 +0300

    Fixed api client interface

--- src/api-client/clients/ApiClientInterface.ts
+++ src/api-client/clients/ApiClientInterface.ts
@@ -4,7 +4,7 @@ import { AxiosResponseHeaders } from "axios/index";
 export interface ApiClientInterface {
     get<T>(endpoint: string, params?: object): Promise<{ data: T, headers: RawAxiosResponseHeaders | AxiosResponseHeaders }>;
     post<T>(endpoint: string, data?: object): Promise<{ data: T, headers: RawAxiosResponseHeaders | AxiosResponseHeaders }>;
-    put(endpoint: string, data?: object): Promise<any>;
+    put<T>(endpoint: string, data?: object): Promise<{ data: T, headers: RawAxiosResponseHeaders | AxiosResponseHeaders }>;
     delete(endpoint: string): Promise<any>;
     uploadFile(endpoint: string, formData: FormData): Promise<any>;
 }
